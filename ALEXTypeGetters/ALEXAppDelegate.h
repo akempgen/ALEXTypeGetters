d3e21a86d807f69a002c9b0868824319c78cb2d6
Alexander Kempgen <alex@kempgen.de>
2013-04-27 12:23:44 +0200

    Initial Commit

---
+++ ALEXTypeGetters/ALEXAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ALEXAppDelegate.h
+//  ALEXTypeGetters
+//
+//  Created by Alexander Kempgen on 27.04.13.
+//  Copyright (c) 2013 Alexander Kempgen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ALEXAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ ALEXTypeGettersTests/ALEXTypeGettersTests.h
@@ -0,0 +1,13 @@
+//
+//  ALEXTypeGettersTests.h
+//  ALEXTypeGettersTests
+//
+//  Created by Alexander Kempgen on 27.04.13.
+//  Copyright (c) 2013 Alexander Kempgen. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ALEXTypeGettersTests : SenTestCase
+
+@end
